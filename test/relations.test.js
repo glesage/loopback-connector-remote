a893820243913c004d1fd525a5b5ebbca6011a5e
Geoffroy <gefthefrench@gmail.com>
2016-01-08 14:14:48 -0600

    Got the first relation test to pass

--- test/relations.test.js
+++ test/relations.test.js
@@ -13,28 +13,22 @@ describe('Relations Tests', function() {
       app: ctx.serverApp,
       datasource: ctx.serverDatasource
     });
+
     ctx.remoteApp = helper.createRestAppAndListen(3002);
+    ctx.remoteDatasource = helper.createRemoteDataSource(ctx.serverApp);
     ctx.RemoteModel = helper.createModel({
       name: 'TestModel',
       app: ctx.remoteApp,
-      datasource: helper.createRemoteDataSource(ctx.serverApp)
+      datasource: ctx.remoteDatasource
     });
 
-    ctx.RelationModel = helper.createModel({
+    ctx.ServerRelationModel = helper.createModel({
       name: 'RelationModel',
       app: ctx.serverApp,
-      datasource: ctx.serverDatasource,
-      options: {
-        relations: {
-          'foo':
-          {
-              type: 'belongsTo',
-              model: 'TestModel',
-              foreignKey: 'TestModelId'
-          }
-        }
-      }
+      datasource: ctx.serverDatasource
     });
+
+    ctx.ServerRelationModel.belongsTo(ctx.ServerModel, {foreignKey: 'testModelId', as: 'TestModel'});
 
     ctx.ServerModel.create({id: 1}, done);
   });
@@ -60,24 +54,24 @@ describe('Relations Tests', function() {
     });
   });
 
-  it('should find all instances of the RelationModel', function(done) {
-    ctx.RelationModel.create([{id: 1, serverModelId: 1}, {id: 2}],
+  it('should find all instances of the ServerRelationModel', function(done) {
+    ctx.ServerRelationModel.create([{id: 1, serverModelId: 1}, {id: 2}],
         function(err, instances) {
-      ctx.RelationModel.find({}, function(err, instances) {
+      ctx.ServerRelationModel.find({}, function(err, instances) {
         assert(instances.length, 1);
-        ctx.RelationModel.destroyAll(done);
+        ctx.ServerRelationModel.destroyAll(done);
       });
     });
   });
 
-  it('should find all instances w/ relations of the RelationModel',
+  it('should find all instances w/ relations of the ServerRelationModel',
       function(done) {
-    ctx.RelationModel.create([{id: 1, serverModelId: 1}, {id: 2}],
+    ctx.ServerRelationModel.create([{id: 1, serverModelId: 1}, {id: 2}],
         function(err, instances) {
-      ctx.RelationModel.find({include: 'foo'}, function(err, instances) {
+      ctx.ServerRelationModel.find({include: 'TestModel'}, function(err, instances) {
         assert(instances.length, 2);
         instances.forEach(function(i) {
-          if (i.id === 1) assert(i.foo.length, 1);
+          if (i.id === 1) assert(i.TestModel);
         });
         done();
       });
